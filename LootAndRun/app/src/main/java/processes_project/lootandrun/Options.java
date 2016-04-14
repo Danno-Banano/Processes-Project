377e2a0ae6584f4f7e253378f6ed845876cf7d99
Charles Steiner <danny.steiner1@gmail.com>
2016-04-14 19:21:17 -0400

    Altered options menu to have actual options

--- LootAndRun/app/src/main/java/processes_project/lootandrun/Options.java
+++ LootAndRun/app/src/main/java/processes_project/lootandrun/Options.java
@@ -1,11 +1,9 @@
 package processes_project.lootandrun;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import java.util.ArrayList;
 
 public class Options extends AppCompatActivity {
 
@@ -13,18 +11,11 @@ public class Options extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void progressReset() {
+        ArrayList<Item> emptyInventory = new ArrayList<>();
+        MainMap.getMainPlayer().setHealth(100);
+        MainMap.getMainPlayer().setInventory(emptyInventory);
+    }
 }
