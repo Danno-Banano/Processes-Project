cc6a3cd6316a44def981b43abca464f4875e2c01
Charles Steiner <danny.steiner1@gmail.com>
2016-04-15 17:15:38 -0400

    Added loot display to victory screen

--- LootAndRun/app/src/main/java/processes_project/lootandrun/VictoryScreen.java
+++ LootAndRun/app/src/main/java/processes_project/lootandrun/VictoryScreen.java
@@ -1,19 +1,29 @@
 package processes_project.lootandrun;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class VictoryScreen extends AppCompatActivity {
 
+    public ArrayList<Item> playerInventory = MainMap.getMainPlayer().getInventory();
+    TextView loot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victory_screen);
+        loot = (TextView) (findViewById(R.id.lootText));
+        displayLootz();
     }
 
+    public void displayLootz() {
+        loot.setText("You collected a " + playerInventory.get(playerInventory.size()-1) + " from the corpse.");
 
+    }
     public void returnToMap(View view)
     {
         finish();
